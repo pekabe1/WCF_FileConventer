6df52028213dd143224a07297dd02e3ac6242c10
Kacper Ziarkowski <k.ziarkowski@microsoft.wsei.edu.pl>
2019-10-08 18:10:56 +0200

    Add project files.

---
+++ WCF_FileConventer/FileConventerService.svc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCF_FileConventer
+{
+    public class FileConventerService : IFileConventerService
+    {
+        public FileModel ConvertToCSV(FileModel file)
+        {
+            var name = file.Name.Replace(".txt", ".csv");
+            var content = file.Content.
+                Replace("|", ";").
+                Replace(",",Environment.NewLine);
+            return new FileModel
+            {
+                Name = name,
+                Content = content
+            };
+        }
+
+        public FileModel GetDataUsingDataContract(FileModel composite)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WCF_FileConventer/IFileConventerService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCF_FileConventer
+{
+
+    [ServiceContract]
+    public interface IFileConventerService
+    {
+
+        [OperationContract]
+        FileModel ConvertToCSV(FileModel file);
+
+        [OperationContract]
+        FileModel GetDataUsingDataContract(FileModel composite);
+        // TODO: Add your service operations here
+    }
+
+    [DataContract] // model
+    public class FileModel
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Content { get; set; }
+    }
+}
